fff776e82679163f3daa3862a88ece6804229dd0
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-14 02:08:05 +0800

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import Home from "./routes/Home"
 import Header from "./components/header"
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignInPage from "./routes/SignIn";
 import SignUpPage from "./routes/SignUp";
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
-        {/* <Header /> */}
+        <Header />
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/sign-in" element={<SignInPage/>} />
